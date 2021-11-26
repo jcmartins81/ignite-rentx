8841a873b85b5136df77bb10e9f7730860f9d5e0
José Carlos Martins <jcmartins81@outlook.com>
2021-11-25 23:11:39 -0300

    chore: add categories.routes.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import {categoriesRoutes} from "./routes/categories.routes";
 
 const app = express()
 
@@ -8,5 +9,7 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+app.use(categoriesRoutes)
 
 export default app
