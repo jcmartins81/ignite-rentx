5075e3093b966f77c400f76cae5ca89674ddfd72
José C. Martins <jcmartins81@outlook.com>
2021-11-29 23:43:27 -0300

    refactor: specifications.router.ts

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,6 +1,4 @@
 import {Router}from "express"
-import CreateSpecificationUseCase from "../modules/cars/useCases/specifications/createSpecification/CreateSpecificationUseCase";
-import SpecificationsRepository from "../modules/cars/repositories/implementations/SpecificationsRepository";
 import {createSpecificationController} from "../modules/cars/useCases/specifications/createSpecification";
 
 const specificationsRoutes = Router()
